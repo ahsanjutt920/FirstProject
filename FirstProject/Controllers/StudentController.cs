1f10735fc45d4aac7e50f08656ca2308254aa895
DELL <DELL@DESKTOP-HQ3GDM0>
2018-09-25 23:57:58 -0700

    Student Controller,Add Student ,Add Teacher

---
+++ FirstProject/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        private FprojectContext ORM = null;
+        public StudentController(FprojectContext ORM)
+        {
+            this.ORM = ORM;
+        }
+        [HttpGet]
+        public IActionResult AddStudent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddStudent(Student S)
+        {
+            ORM.Student.Add(S);
+            ORM.SaveChanges();
+            ViewBag.Messege = "new Student Added Successfullly";
+            return View();
+        }
+        public IActionResult AddTeacher()
+        {
+            return View();
+        }
+    }
+}
