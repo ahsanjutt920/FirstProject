bbb003147a6f6ceabb91e854c1a9f1c59c88ea1d
DELL <DELL@DESKTOP-HQ3GDM0>
2018-09-25 18:22:41 -0700

    Add project files.

---
+++ FirstProject/Models/FprojectContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FirstProject.Models
+{
+    public partial class FprojectContext : DbContext
+    {
+        public FprojectContext()
+        {
+        }
+
+        public FprojectContext(DbContextOptions<FprojectContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Student { get; set; }
+        public virtual DbSet<Teacher> Teacher { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-HQ3GDM0;DataBase=Fproject;Trusted_Connection=True; User ID=sa; Password=ahsan;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Address).HasMaxLength(50);
+
+                entity.Property(e => e.ContactNo)
+                    .HasColumnName("Contact_No")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Cv).HasMaxLength(50);
+
+                entity.Property(e => e.Dept).HasMaxLength(50);
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Teacher>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Address).HasMaxLength(50);
+
+                entity.Property(e => e.ContactNo)
+                    .HasColumnName("Contact_No")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Dept).HasMaxLength(50);
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+        }
+    }
+}
